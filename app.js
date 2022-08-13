144c71f113c5b2e9e8472f13cb50653af267ec5e
OTAVIANO_SILVERIO_DE_SOUSA <otaviano@DELTAB47.BARROCA>
2022-08-13 11:20:44 -0300

    Teste app.js

--- app.js
+++ app.js
@@ -3,3 +3,5 @@ Console.log("Tô com sono!")
 Console.log("Dorme amor")
 
 Console.log("*Barulho notificação do whatsapp* Oii bia! Td bem?")
+
+Console.log("Cala a boca, namorada do alecrim!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
